fc780da2b421672c211cc859ea633db05bf9d136
Stanislavlav <76530125+Stanislavlav@users.noreply.github.com>
2021-02-17 21:34:03 +0400

    Update praktika_urok4.py
    Ver_2

--- praktika_urok4.py
+++ praktika_urok4.py
@@ -5,3 +5,9 @@ tretie = int(input('ввести третье число'))
 proizvod =(pervoe * vtoroe * tretie)
 print(proizvod)
 
+#Задача 1
+num1 = input('введите первое число') #Вводим первое число
+num2 = input('введите второе число') #Вводим второе число
+num3 = input('введите третье число') #Вводим третье число
+print(num1 + num2 + num3)
+
