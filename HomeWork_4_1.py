763ec99afb7641c1452c1bad96284e91739fa7d8
Stanislavlav <76530125+Stanislavlav@users.noreply.github.com>
2021-02-25 21:21:13 +0400

    Work

---
+++ HomeWork_4_1.py
@@ -0,0 +1,10 @@
+a = float(input("Введите число в метрах: "))
+
+s = a * 100
+d = a * 10
+mm = a * 1000
+mil = a / 1609
+print(str(s) + " " "см")
+print(str(d) + " " "дц")
+print(str(mm) + " " "мм")
+print(str(mil) + " " "мил")
